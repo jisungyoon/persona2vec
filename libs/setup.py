41e9aefe985c33d965bfa0df0c892e8e6a135818
jisungyoon <balla2081@postech.ac.kr>
2019-08-29 04:48:07 +0900

    add_libs

---
+++ libs/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+ 
+ 
+ 
+setup(name='persona2vec',
+      version='0.1',
+      url='https://github.com/jisungyoon/persona2vec',
+      license='MIT',
+ 
+      author='Jisung Yoon, Kaicheng Yang and Yong-Yeol Ahn',
+      author_email='jisung.yoon92@gmail.com',
+      description='Persona2Vec library',
+      packages=find_packages(),
+      zip_safe=False)
