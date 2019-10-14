a883f59b3fc1973eafc8ad41b7cba95e6cfc161e
jisungyoon <balla2081@postech.ac.kr>
2019-10-15 04:47:42 +0900

    parallelize script

--- libs/persona2vec/link_prediction.py
+++ libs/persona2vec/link_prediction.py
@@ -27,8 +27,8 @@ class linkPredictionTask(object):
         self.walk_length = walk_length
         self.dimensions = dimensions
         self.window_size = window_size
-		self.workers = workers
-		self.base_iter = base_iter
+        self.workers = workers
+        self.base_iter = base_iter
 
         self.original_edge_set = set(G.edges)
         self.node_list = list(G.nodes)
@@ -94,9 +94,9 @@ class linkPredictionTask(object):
 
     def calculate_ROC_AUC_value(self):
         logging.info('Calcualte ROC_AUC values')
-		y_true = np.concatenate([np.ones_like(test_object.link_prediction_score_postive), np.zeros_like(test_object.link_prediction_score_negative)])
-		y_score = np.concatenate([test_object.link_prediction_score_postive, test_object.link_prediction_score_negative], axis=0)
-		self.ROC_AUC_value = roc_auc_score(y_true, y_score)
+        y_true = np.concatenate([np.ones_like(test_object.link_prediction_score_postive), np.zeros_like(test_object.link_prediction_score_negative)])
+        y_score = np.concatenate([test_object.link_prediction_score_postive, test_object.link_prediction_score_negative], axis=0)
+        self.ROC_AUC_value = roc_auc_score(y_true, y_score)
 
     def print_result(self):
         logging.info(self.name)
