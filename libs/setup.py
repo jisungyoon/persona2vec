60bfe3bddb5566e62b5aee2ad460b3368feb2eaa
YY Ahn <yongyeol@gmail.com>
2019-10-15 08:26:28 -0400

    indentation fix

--- libs/setup.py
+++ libs/setup.py
@@ -5,11 +5,11 @@ setup(name='persona2vec',
       version='0.1',
       url='https://github.com/jisungyoon/persona2vec',
       license='MIT',
-      entry_points = {
-        'console_scripts': ['persona2vec=persona2vec.command_line:main'],
-    },
+      entry_points={
+          'console_scripts': ['persona2vec=persona2vec.command_line:main'],
+      },
 
-	  author='Jisung Yoon, Kaicheng Yang and Yong-Yeol Ahn',
+      author='Jisung Yoon, Kaicheng Yang and Yong-Yeol Ahn',
       author_email='jisung.yoon92@gmail.com',
       description='Persona2Vec library',
       packages=find_packages(),
