1c6a9ad2279616bd944231156e78a0523ed4a7f6
balla2081 <balla2081@postech.ac.kr>
2019-10-16 19:02:24 -0400

    generalize link prediction code

--- libs/persona2vec/link_prediction.py
+++ libs/persona2vec/link_prediction.py
@@ -15,46 +15,31 @@ class linkPredictionTask(object):
     def __init__(self, G,
                  test_edges,
                  negative_edges,
+                 embs,
                  name,
-                 lambd=0.1,
-                 num_walks=10,
-                 walk_length=40,
-                 dimensions=16,
-                 window_size=5,
-                 workers=4,
-                 base_iter=5):
+                 persona=False,
+                 node_to_persona={}):
              
         self.G = G
         self.test_edges = test_edges
-        self.negative_edges = negative_edges       
+        self.negative_edges = negative_edges   
+        self.embs = embs
         self.name = name
         
-        self.lambd = lambd
-        self.num_walks = num_walks
-        self.walk_length = walk_length
-        self.dimensions = dimensions
-        self.window_size = window_size
-        self.workers = workers
-        self.base_iter = base_iter
-
-    def learn_persona2vec_emb(self):
-        logging.info('Initiate persona2vec')
-        self.model = Persona2Vec(self.G,
-                                 lambd=self.lambd,
-                                 num_walks=self.num_walks,
-                                 walk_length=self.walk_length,
-                                 dimensions=self.dimensions,
-                                 window_size=self.window_size,
-                                 workers=self.workers,
-                                 base_iter=self.base_iter)
-        self.model.simulate_walks()
-        self.emb = self.model.learn_embedding()
-
+        self.persona = persona
+        self.node_to_persona = node_to_persona
+        
+    def do_link_prediction(self):
+        self.calculate_link_prediction_score()
+        self.calculate_ROC_AUC_value()
+        self.print_result()
+    
     def calculate_link_prediction_score(self):
+        caculate_method = self.calculate_score_persona if persona else self.calculate_score
         self.link_prediction_score_postive = np.array(
-            self.calculate_score(self.test_edges))
+            calculate_method(self.test_edges))
         self.link_prediction_score_negative = np.array(
-            self.calculate_score(self.negative_edges))
+            calculate_method(self.negative_edges))
 
     def calculate_score(self, edge_list):
         score_list = []
@@ -64,6 +49,10 @@ class linkPredictionTask(object):
             max_sim = max([np.dot(self.emb[src_persona], self.emb[tag_persona])
                            for src_persona in src_personas for tag_persona in tag_personas])
             score_list.append(max_sim)
+        return score_list
+    
+    def calculate_score_persona(self, edge_list):
+        score_list = [np.dot(self.emb[source], self.emb[target]) for source, target in edge_list]
         return score_list
 
     def calculate_ROC_AUC_value(self):
@@ -76,12 +65,9 @@ class linkPredictionTask(object):
 
     def print_result(self):
         logging.info(self.name)
-        logging.info(self.lambd)
-        logging.info(self.dimensions)
         logging.info(self.ROC_AUC_value)
 
     def write_result(self, file_name):
         f = open(file_name, 'a')
-        f.write('\t'.join([self.name, str(self.lambd), str(
-            self.dimensions), str(self.ROC_AUC_value)]) + '\n')
+        f.write('\t'.join([self.name, str(self.ROC_AUC_value)]) + '\n')
         f.close()
