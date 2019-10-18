7d2c293b0c3e2e7f8766602df03ae8ba7086b971
YY Ahn <yongyeol@gmail.com>
2019-10-17 21:26:58 -0400

    minor formatting

--- workflow/script/common_function.py
+++ workflow/script/common_function.py
@@ -1,5 +1,6 @@
 from multiprocessing import Process
 
+
 def run_parallel(function, args, number_of_cores):
     procs = [Process(target=function, args=[proc_num] + args)
              for proc_num in range(number_of_cores)]
