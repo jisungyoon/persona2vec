49549d4c966bc552aab74cddd03c11b2ea3f8eec
jisungyoon <balla2081@postech.ac.kr>
2019-11-07 18:10:47 -0500

    modify algorithm

--- libs/persona2vec/model.py
+++ libs/persona2vec/model.py
@@ -1,4 +1,4 @@
-import pickle
+import json
 import networkx as nx
 
 from persona2vec.node2vec import Node2Vec
@@ -9,7 +9,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 
 
-class Persona2Vec(Node2Vec):
+class Persona2Vec(object):
     """
     Persona2Vec node embedding object
     """
@@ -17,14 +17,18 @@ class Persona2Vec(Node2Vec):
     def __init__(self, G,
                  lambd=0.1,
                  directed=False,
-                 num_walks=10,
-                 walk_length=40,
+                 num_walks_base=10,
+                 walk_length_base=40,
+                 window_size_base=5,
+                 num_walks_persona=5,
+                 walk_length_persona=80,
+                 window_size_persona=2,
                  p=1.0,
                  q=1.0,
                  dimensions=128,
-                 window_size=5,
-                 base_iter=5,
-                 workers=1):
+                 epoch_base=1,
+                 epoch_persona=1,
+                 workers=4):
         """
         :param G: NetworkX graph object. persona graah
         :param lambd: edge weight for persona edge, usually 0 ~ 1
@@ -40,7 +44,22 @@ class Persona2Vec(Node2Vec):
         """
         self.original_network = G
         self.lambd = lambd
-
+        
+        # Get base embeddings on the origin network
+        self.base_model = Node2Vec(self.original_network,
+                                     directed=directed,
+                                     num_walks=num_walks_base,
+                                     walk_length=walk_length_base,
+                                     p=p,
+                                     q=q,
+                                     dimensions=dimensions,
+                                     window_size=window_size_base,
+                                     epoch=epoch_base,
+                                     workers=workers)
+        self.base_model.simulate_walks()
+        self.base_embedding = self.base_model.learn_embedding()
+        
+        # Generate persona network
         splitter = EgoNetSplitter(
             self.original_network, directed=directed, lambd=self.lambd)
         self.persona_network = splitter.persona_network
@@ -48,17 +67,20 @@ class Persona2Vec(Node2Vec):
         self.persona_to_node = splitter.personality_map
         del splitter
 
-        # generating Node2vec instant
-        super().__init__(self.persona_network,
-                         directed=directed,
-                         num_walks=num_walks,
-                         walk_length=walk_length,
+        # Get persona embedding with base embedding
+        self.persona_model = Node2Vec(self.persona_network,
+                         directed=True,
+                         num_walks=num_walks_persona,
+                         walk_length=walk_length_persona,
                          p=p,
                          q=q,
                          dimensions=dimensions,
-                         window_size=window_size,
-                         base_iter=base_iter,
+                         window_size=window_size_persona,
+                         epoch=epoch_persona,
                          workers=workers)
+        self.persona_model.simulate_walks()
+        self.persona_model.initialize_persona_vectors(self.base_embedding, self.persona_to_node)
+        self.embedding = self.persona_model.learn_embedding_one_epoch()
 
     def save_persona_network(self, file_path):
         """
@@ -72,13 +94,13 @@ class Persona2Vec(Node2Vec):
         :param file_path: file_path for persona to node mapper
         :return:
         """
-        with open(file_path, 'wb') as f:
-            pickle.dump(self.persona_to_node, f)
+        with open(file_path, 'w') as f:
+            json.dump(self.persona_to_node, f)
 
     def save_node_to_persona_mapping(self, file_path):
         """
         :param file_path: file_path for node to persona mapper
         :return:
         """
-        with open(file_path, 'wb') as f:
-            pickle.dump(self.node_to_persona, f)
+        with open(file_path, 'w') as f:
+            json.dump(self.node_to_persona, f)
