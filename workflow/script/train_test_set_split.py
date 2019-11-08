f89a44623f784cf1c201f9c924e0e488813a7c52
jisungyoon <balla2081@postech.ac.kr>
2019-11-08 13:18:36 -0500

    using py3 string formatting

--- workflow/script/train_test_set_split.py
+++ workflow/script/train_test_set_split.py
@@ -9,13 +9,14 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
 
 def train_test_set_split(IN_FILE, INDEX, DIRECTED):
     G = read_graph(IN_FILE, directed=DIRECTED)
-    OUTPUT_PATH = IN_FILE.split(".")[0] + "_" + str(INDEX)
+    OUTPUT_PATH = "{}_{}".format(IN_FILE.split(".")[0], INDEX)
     splitter = NetworkTrainTestSplitter(G, directed=DIRECTED)
     splitter.train_test_split()
     splitter.generate_negative_edges()
     splitter.save_splitted_result(OUTPUT_PATH)
 
 
+name = "{}-{}".format(node, i + 1)
 if __name__ == "__main__":
     IN_FILE = sys.argv[1]
     INDEX = sys.argv[2]
