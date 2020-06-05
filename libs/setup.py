0819e19a14381ee63261211c2265aca145a50da4
jisungyoon <balla2081@postech.ac.kr>
2020-06-05 14:31:22 -0400

    de identification

--- libs/setup.py
+++ libs/setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 
 setup(name='persona2vec',
       version='0.1',
-      url='https://github.com/jisungyoon/persona2vec',
+      url='(anon),
       license='MIT',
       entry_points={
           'console_scripts': ['persona2vec=persona2vec.command_line:main'],
       },
 
-      author='Jisung Yoon, Kaicheng Yang and Yong-Yeol Ahn',
-      author_email='jisung.yoon92@gmail.com',
+      author='Anonymous Authors',
+      author_email='anon',
       description='Persona2Vec library',
       packages=find_packages(),
       zip_safe=False)
