8501e762234cb48aa8426b47c1d9d48aeff8f3db
Sadamori Kojaku <skojaku@iu.edu>
2022-04-27 22:22:47 -0400

    add a test script

--- libs/persona2vec_gcn/tests/test.py
+++ libs/persona2vec_gcn/tests/test.py
@@ -1,5 +1,6 @@
 import unittest
 
+import persona2vec_gcn as pvgcn
 from persona2vec_gcn.model import Persona2VecGCN
 from persona2vec_gcn.vgae import DeepVGAE
 import numpy as np
@@ -12,22 +13,34 @@ class TestPersona2VecGCN(unittest.TestCase):
 
     def test_embedding(self):
         dimensions = 64
-        model = Persona2VecGCN(self.G, X = self.A.toarray(), lambd=0.1, dimensions = dimensions)
+        X = pvgcn.laplacian_eigenmap(self.G, dimensions = 16)
+        model = Persona2VecGCN(self.G, X = X, lambd=0.1, dimensions = dimensions)
         emb = np.vstack(model.embedding.values())
         assert emb.shape[1] == dimensions
         assert emb.shape[0] >= self.A.shape[0]
 
     def test_splitting_algorithm(self):
         dimensions = 64
-        model = Persona2VecGCN(self.G, X = self.A.toarray(), lambd=0.1, dimensions = dimensions, clustering_method="modularity")
-        model = Persona2VecGCN(self.G, X = self.A.toarray(), lambd=0.1, dimensions = dimensions, clustering_method="label_prop")
+        X = pvgcn.laplacian_eigenmap(self.G, dimensions = 16)
+        model = Persona2VecGCN(self.G, X = X, lambd=0.1, dimensions = dimensions, clustering_method="modularity")
+        model = Persona2VecGCN(self.G, X = X, lambd=0.1, dimensions = dimensions, clustering_method="label_prop")
 
     def test_deepvgae(self):
         dimensions = 64
-        model = DeepVGAE(self.G, dimensions=dimensions)
+        X = pvgcn.laplacian_eigenmap(self.G, dimensions = 16)
+        model = DeepVGAE(self.G, X = X, dimensions=dimensions)
         model.learn_embedding()
         emb = np.vstack(model.embedding.values())
         assert emb.shape[1] == dimensions
 
+    def test_laplacian_eigenmap(self):
+        dimensions = 5
+        emb = pvgcn.laplacian_eigenmap(self.G, dimensions = dimensions)
+        assert emb.shape[1] == dimensions
+
+    def test_fastRP(self):
+        dimensions = 5
+        emb = pvgcn.fastRP(self.G, dimensions = dimensions)
+        assert emb.shape[1] == dimensions
 if __name__ == "__main__":
     unittest.main()
